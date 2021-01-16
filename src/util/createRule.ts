1cc0102cfadd26cfb82fc2c5838d6bb2ecf63c55
Vince Au <vinceau09@gmail.com>
2021-01-16 11:52:55 +1100

    Remove createRule util

--- src/util/createRule.ts
+++
@@ -1,6 +0,0 @@
-import { ESLintUtils } from "@typescript-eslint/experimental-utils";
-
-// note - cannot migrate this to an import statement because it will make TSC copy the package.json to the dist folder
-// const version = require('../../package.json').version;
-
-export const createRule = ESLintUtils.RuleCreator(() => `https://github.com/vinceau/no-nullable-numbers`);
